1213ba9a794582f68b9cd7d4eac0aebcfc7ee3c9
Nikolay Lyubavin <nik.lubavin@gmail.com>
2020-03-19 19:44:15 +0300

    Updates

---
+++ precache-manifest.5baff125eb405e9769aa69486d03c1cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7472f1b4a2a920324a62357841c91936",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "90bb762ebd2f1743875a",
+    "url": "/todo-list/static/css/main.8ec6f1a6.chunk.css"
+  },
+  {
+    "revision": "76a19de90f9ffe585802",
+    "url": "/todo-list/static/js/2.93276ebd.chunk.js"
+  },
+  {
+    "revision": "94b03e19a4c9eb66db847f3cca1e03ba",
+    "url": "/todo-list/static/js/2.93276ebd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90bb762ebd2f1743875a",
+    "url": "/todo-list/static/js/main.38fd2080.chunk.js"
+  },
+  {
+    "revision": "16dc963751aa5234562c",
+    "url": "/todo-list/static/js/runtime-main.57a4dc61.js"
+  }
+]);
